5a1f7c7f89d2c4eb7e2879e1819ee1169c27f661
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-04-30 17:28:28 +1000

    Output -> Item

--- src/slices/mod.rs
+++ src/slices/mod.rs
@@ -2,19 +2,19 @@ use core::ops::Range;
 
 pub trait SliceByValue {
     /// See [the `Index` implementation for slices](slice#impl-Index%3CI%3E-for-%5BT%5D).
-    fn index_value<I: SliceByValueIndex<Self>>(&self, index: I) -> I::Output {
+    fn index_value<I: SliceByValueIndex<Self>>(&self, index: I) -> I::Item {
         index.index_value(self)
     }
 
     /// See [`slice::get_unchecked`].
     ///
     /// For a safe alternative see [`SliceByValue::get_value`].
-    unsafe fn get_value_unchecked<I: SliceByValueIndex<Self>>(&self, index: I) -> I::Output {
+    unsafe fn get_value_unchecked<I: SliceByValueIndex<Self>>(&self, index: I) -> I::Item {
         unsafe { index.get_value_unchecked(self) }
     }
 
     /// See [`slice::get`].
-    fn get_value<I: SliceByValueIndex<Self>>(&self, index: I) -> Option<I::Output> {
+    fn get_value<I: SliceByValueIndex<Self>>(&self, index: I) -> Option<I::Item> {
         index.get_value(self)
     }
 
@@ -35,32 +35,28 @@ pub trait SliceByValueMut: SliceByValue {
     unsafe fn set_value_unchecked<I: SliceByValueMutIndex<Self>>(
         &mut self,
         index: I,
-        value: I::Output,
-    ) -> I::Output {
+        value: I::Item,
+    ) -> I::Item {
         unsafe { index.set_value_unchecked(self, value) }
     }
 
     /// Sets the value at the given index to the given value and
     /// returns the previous value.
-    fn set_value<I: SliceByValueMutIndex<Self>>(
-        &mut self,
-        index: I,
-        value: I::Output,
-    ) -> I::Output {
+    fn set_value<I: SliceByValueMutIndex<Self>>(&mut self, index: I, value: I::Item) -> I::Item {
         index.set_value(self, value)
     }
 }
 
 pub trait SliceByValueIndex<S: SliceByValue + ?Sized> {
-    type Output;
-    fn get_value(&self, slice: &S) -> Option<Self::Output>;
-    fn index_value(&self, slice: &S) -> Self::Output;
-    unsafe fn get_value_unchecked(&self, slice: &S) -> Self::Output;
+    type Item;
+    fn get_value(&self, slice: &S) -> Option<Self::Item>;
+    fn index_value(&self, slice: &S) -> Self::Item;
+    unsafe fn get_value_unchecked(&self, slice: &S) -> Self::Item;
 }
 
 pub trait SliceByValueMutIndex<S: SliceByValueMut + ?Sized>: SliceByValueIndex<S> {
-    fn set_value(&self, slice: &mut S, value: Self::Output) -> Self::Output;
-    unsafe fn set_value_unchecked(&self, slice: &mut S, value: Self::Output) -> Self::Output;
+    fn set_value(&self, slice: &mut S, value: Self::Item) -> Self::Item;
+    unsafe fn set_value_unchecked(&self, slice: &mut S, value: Self::Item) -> Self::Item;
 }
 
 /// Convenience trait for specifying the behavior of a
@@ -68,14 +64,14 @@ pub trait SliceByValueMutIndex<S: SliceByValueMut + ?Sized>: SliceByValueIndex<S
 ///
 /// This traits makes it possible to write trait bounds as
 /// ```ignore
-/// T: IndexableBy<usize, Output = int32>
+/// T: IndexableBy<usize, Item = int32>
 /// ```
 /// instead of the equivalent `where` clause
 /// ```ignore
-/// where T: SliceByValue, usize: SliceByValueIndex<Output = i32>
+/// where T: SliceByValue, usize: SliceByValueIndex<Item = i32>
 /// ```
 pub trait IndexableBy<I>: SliceByValue {
-    type Output;
+    type Item;
 }
 
 impl<I, T> IndexableBy<I> for T
@@ -83,11 +79,22 @@ where
     I: SliceByValueIndex<T>,
     T: SliceByValue,
 {
-    type Output = I::Output;
+    type Item = I::Item;
 }
 
+/// Convenience trait for specifying the behavior of a
+/// mutable by-value slice.
+///
+/// This traits makes it possible to write trait bounds as
+/// ```ignore
+/// T: IndexableByMut<usize, Item = int32>
+/// ```
+/// instead of the equivalent `where` clause
+/// ```ignore
+/// where T: SliceByValueMut, usize: SliceByValueMutIndex<Item = i32>
+/// ```
 pub trait IndexableByMut<I>: SliceByValueMut {
-    type Output;
+    type Item;
 }
 
 impl<I, T> IndexableByMut<I> for T
@@ -95,5 +102,5 @@ where
     I: SliceByValueMutIndex<T>,
     T: SliceByValueMut,
 {
-    type Output = I::Output;
+    type Item = I::Item;
 }
