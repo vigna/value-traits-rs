cba110c44d45e6f5a42be82924a0d7af137c135e
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2025-05-02 09:07:19 +0200

    test

--- tests/test_slices.rs
+++ tests/test_slices.rs
@@ -1,11 +1,17 @@
+use core::ops::Range;
 use value_traits_rs::slices::SliceByValue;
 
 #[test]
 fn test_slices() {
     let s = vec![1_i32, 2, 3];
     assert_eq!(test(s.as_slice()), 1);
+    assert_eq!(test2(s.as_slice()), &[1, 2]);
 }
 
 fn test(s: impl SliceByValue<usize, Value = i32>) -> i32 {
     s.index_value(0_usize)
+}
+
+fn test2<'a>(s: impl SliceByValue<Range<usize>, Value = &'a [i32]>) -> &'a [i32] {
+    s.index_value(0..2)
 }
