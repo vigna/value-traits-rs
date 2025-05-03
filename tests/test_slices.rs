aa5ba07691ffc5186dc75b3ffd8f6837d1dba3d0
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-05-03 11:44:18 +1000

    Problems with len

--- tests/test_slices.rs
+++ tests/test_slices.rs
@@ -4,14 +4,27 @@ use value_traits_rs::slices::SliceByValue;
 #[test]
 fn test_slices() {
     let s = vec![1_i32, 2, 3];
-    assert_eq!(test(s.as_slice()), 1);
-    assert_eq!(test2(s.as_slice()), &[1, 2]);
+    assert_eq!(test_usize(s.as_slice()), 1);
+    assert_eq!(test_range(s.as_slice()), &[1, 2]);
+    assert_eq!(test_usize_range(s.as_slice()), (1, [1, 2].as_ref()));
 }
 
-fn test(s: impl SliceByValue<usize, Value = i32>) -> i32 {
+fn test_usize(s: impl SliceByValue<usize, Value = i32>) -> i32 {
     s.index_value(0_usize)
 }
 
-fn test2<'a>(s: impl SliceByValue<Range<usize>, Value = &'a [i32]>) -> &'a [i32] {
+fn test_range<'a>(s: impl SliceByValue<Range<usize>, Value = &'a [i32]>) -> &'a [i32] {
     s.index_value(0..2)
+}
+
+fn test_usize_range<'a>(
+    s: impl SliceByValue<usize, Value = i32> + SliceByValue<Range<usize>, Value = &'a [i32]>,
+) -> (i32, &'a [i32]) {
+    (s.index_value(0_usize), s.index_value(0..2))
+}
+
+fn test_les<'a>(
+    s: impl SliceByValue<usize, Value = i32> + SliceByValue<Range<usize>, Value = &'a [i32]>,
+) -> usize {
+    s.len()
 }
